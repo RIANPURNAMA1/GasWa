29a9d1487360d30903240f724ec1730cc26d74e0
Rian Purnama <rianprnma7@gmail.com>
2026-02-09 14:01:09 +0700

    peubhan

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,12 @@ const mysql = require("mysql2");
 
 // CREATE POOL (WAJIB untuk server production)
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "wa_gateway",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: 0,
 });
 
